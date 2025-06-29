fa510f7286fa750d64def74741d51bf6a893aee4
CodeSleek00 <codesleekstudio@gmail.com>
2025-06-29 23:27:11 +0530

    favicons

--- orders/cart.php
+++ orders/cart.php
@@ -68,6 +68,8 @@ $stmt->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" type="image/png" href="images/Pyaara Circle.png">
+    <link rel="apple-touch-icon" href="images/Pyaara Circle.png">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
--- orders/cutie.php
+++ orders/cutie.php
@@ -10,6 +10,9 @@ $products = $conn->query("SELECT * FROM products WHERE page = '$page' ORDER BY i
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <link rel="icon" type="image/png" href="images/Pyaara Circle.png">
+    <link rel="apple-touch-icon" href="images/Pyaara Circle.png">
   <title><?php echo ucfirst(str_replace('.php', '', $page)); ?> | Pyaara Store</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
