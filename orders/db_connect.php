bf4818a1931f5ec6e63ac8a70002ad3120d131b2
CodeSleek00 <codesleekstudio@gmail.com>
2025-08-28 15:22:27 +0530

    Update db_connect.php

--- orders/db_connect.php
+++ orders/db_connect.php
@@ -1,14 +1,18 @@
-<?php
-// Start session for managing user cart and other session data
-session_start();
 
+<?php
+// db_connect.php
 $host = "178.16.136.97";
 $user = "u298112699_Anant";
 $pass = "Pyaara_store15";
-$db = "u298112699_pyaara_store_A";
+$db   = "u298112699_pyaara_store_A";
 
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!isset($GLOBALS['db'])) {
+    $GLOBALS['db'] = new mysqli($host, $user, $pass, $db);
+
+    if ($GLOBALS['db']->connect_error) {
+        die("Database connection failed: " . $GLOBALS['db']->connect_error);
+    }
 }
+
+$conn = $GLOBALS['db'];
 ?>
