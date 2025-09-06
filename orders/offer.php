19ad906a2af7c0f86b009af3b4a49c282e63ce7c
CodeSleek00 <codesleekstudio@gmail.com>
2025-09-06 20:44:35 +0530

    cart

--- orders/offer.php
+++ orders/offer.php
@@ -307,9 +307,18 @@ if ($products && $products->num_rows > 0) {
                                 <span class="original-price">₹<?php echo number_format($row['original_price'], 2); ?></span>
                                 <span class="discount-percent">Save <?php echo number_format($row['discount_percent'], 0); ?>%</span>
                             </div>
-                            <div class="product-actions">
-                                <a href="product_detail.php?id=<?php echo $row['id']; ?>" class="btn btn-secondary">Buy Now</a>
-                            </div>
+                           <div class="product-actions">
+  <a href="product_detail.php?id=<?php echo $row['id']; ?>" class="btn btn-secondary">Buy Now</a>
+
+  <form method="POST" action="add_to_cart.php" style="flex:1;">
+    <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
+    <input type="hidden" name="quantity" value="1">
+    <button type="submit" class="btn btn-primary">
+      <i class="fas fa-cart-plus"></i> Add to Cart
+    </button>
+  </form>
+</div>
+
                         </div>
                     </div>
                 <?php endforeach; ?>
