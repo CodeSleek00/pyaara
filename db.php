62180185f22dbe1c39aa084a97c8661c336ebdf5
CodeSleek00 <codesleekstudio@gmail.com>
2026-03-13 11:42:51 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$host = "srv1234.hostinger.com"; // Hostinger MySQL host
+$host = "srv1259.hstgr.io"; // Hostinger MySQL host
 $user = "u298112699_Anant";
 $pass = "Pyaara_store15";
 $db   = "u298112699_pyaara_store_A";
