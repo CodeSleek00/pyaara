83608d25ba2ae7dfd922790217e2ddf278b87ae8
CodeSleek00 <codesleekstudio@gmail.com>
2026-03-13 11:41:56 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
+$host = "srv1234.hostinger.com"; // Hostinger MySQL host
 $user = "u298112699_Anant";
 $pass = "Pyaara_store15";
 $db   = "u298112699_pyaara_store_A";
 
-$conn = new mysqli($host,$user,$pass,$db);
+$conn = new mysqli($host, $user, $pass, $db);
 
-if($conn->connect_error){
-    die("Connection failed: ".$conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
