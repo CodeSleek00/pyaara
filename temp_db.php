8e06ce729f623d25046f719931c1f637b833775f
CodeSleek00 <codesleekstudio@gmail.com>
2026-04-12 13:40:25 +0530

    Update temp_db.php

--- temp_db.php
+++ temp_db.php
@@ -1,17 +1,8 @@
 <?php
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
-    // Local
-    $host = "127.0.0.1";
-    $user = "root";
-    $pass = "";
-    $db   = "pyaara_store";
-} else {
-    // Live
-    $host = "srv1259.hstgr.io";
-    $user = "u298112699_Anant";
-    $pass = "Pyaara_store15";
-    $db   = "u298112699_pyaara_store_A";
-}
+$host = "srv1259.hstgr.io"; // Hostinger MySQL host
+$user = "u298112699_Anant";
+$pass = "Pyaara_store15";
+$db   = "u298112699_pyaara_store_A";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
